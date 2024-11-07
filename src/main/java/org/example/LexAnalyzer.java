4e95dd613b972617bb428935828ffd463cd451b3
Wondupper <vanchikwork@gmail.com>
2024-11-07 20:37:49 +0400

    .

---
+++ src/main/java/org/example/LexAnalyzer.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LexAnalyzer {
+    public static List<Token> lex(String input) {
+        List<Token> tokens = new ArrayList<>();
+        StringBuilder currentToken = new StringBuilder();
+        TokenType currentState = null;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            switch (currentState) {
+                case null:
+                    if (Character.isWhitespace(c)) {
+                        continue;
+                    } else if (Character.isLetter(c)) {
+                        currentToken.append(c);
+                        currentState = TokenType.IDENTIFIER;
+                    } else if (Character.isDigit(c)) {
+                        currentToken.append(c);
+                        currentState = TokenType.CONSTANT;
+                    } else if ("<>=!".indexOf(c) != -1) {
+                        currentToken.append(c);
+                        currentState = TokenType.COMPARISON_OPERATOR;
+                    } else if ("+-*/".indexOf(c) != -1) {
+                        tokens.add(new Token(TokenType.UNKNOWN, String.valueOf(c)));
+                    }
+                    break;
+
+                case IDENTIFIER:
+                    if (Character.isLetterOrDigit(c)) {
+                        currentToken.append(c);
+                    } else {
+                        tokens.add(createKeywordOrIdentifierToken(currentToken.toString()));
+                        currentToken.setLength(0);
+                        currentState = null;
+                        i--;
+                    }
+                    break;
+
+                case CONSTANT:
+                    if (Character.isDigit(c)) {
+                        currentToken.append(c);
+                    } else {
+                        tokens.add(new Token(TokenType.CONSTANT, currentToken.toString()));
+                        currentToken.setLength(0);
+                        currentState = null;
+                        i--;
+                    }
+                    break;
+
+                case COMPARISON_OPERATOR:
+                    if ("<>=!".indexOf(c) != -1) {
+                        currentToken.append(c);
+                    } else {
+                        tokens.add(new Token(TokenType.COMPARISON_OPERATOR, currentToken.toString()));
+                        currentToken.setLength(0);
+                        currentState = null;
+                        i--;
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + currentState);
+            }
+        }
+
+        if (currentToken.length() > 0) {
+            if (currentState == TokenType.IDENTIFIER) {
+                tokens.add(createKeywordOrIdentifierToken(currentToken.toString()));
+            } else if (currentState == TokenType.CONSTANT) {
+                tokens.add(new Token(TokenType.CONSTANT, currentToken.toString()));
+            } else if (currentState == TokenType.COMPARISON_OPERATOR) {
+                tokens.add(new Token(TokenType.COMPARISON_OPERATOR, currentToken.toString()));
+            }
+        }
+
+        return tokens;
+    }
+
+    private static Token createKeywordOrIdentifierToken(String text) {
+        switch (text) {
+            case "if": return new Token(TokenType.IF, text);
+            case "then": return new Token(TokenType.THEN, text);
+            case "else": return new Token(TokenType.ELSE, text);
+            case "elseif": return new Token(TokenType.ELSEIF, text);
+            case "end": return new Token(TokenType.END, text);
+            case "output": return new Token(TokenType.OUTPUT, text);
+            default: return new Token(TokenType.IDENTIFIER, text);
+        }
+    }
+
+}
---
+++ src/main/java/org/example/Token.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Token {
+    TokenType type;
+    String value;
+
+    Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Type: "+type +" Value: "+ value;
+    }
+}
